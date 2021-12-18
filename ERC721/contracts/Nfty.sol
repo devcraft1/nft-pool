82e703c14d23ab7b821299ef57d6199831f2009f
devcraft1 <ukonudennis64@gmail.com>
2021-12-17 23:59:20 -1000

    nfty-contract

--- ERC721/contracts/Nfty.sol
+++ ERC721/contracts/Nfty.sol
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Nfty is ERC721URIStorage {
     using Counters for Counters.Counter;
