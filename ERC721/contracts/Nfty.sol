bf717628d83c904f8e49db977e4ed8d364796f81
devcraft1 <ukonudennis64@gmail.com>
2021-12-17 23:49:39 -1000

    nfty-contract

---
+++ ERC721/contracts/Nfty.sol
@@ -0,0 +1,43 @@
+// contracts/Must.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFTY is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    bool private revealed = false;
+    string private revealUrl = "";
+
+    constructor() ERC721("NFTTY", "NFTTIES") {}
+
+    function mintNFT(address collector, string memory _tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(collector, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
+
+        return newItemId;
+    }
+
+
+    function tokenURI(uint tokenId) public view virtual  override returns(string memory){
+          if(revealed ==true){
+              return super.tokenURI(tokenId);
+          }else{
+              return revealUrl;
+          }
+    }
+
+    function revealCollection () public {
+         revealed = true;
+    }
+}
